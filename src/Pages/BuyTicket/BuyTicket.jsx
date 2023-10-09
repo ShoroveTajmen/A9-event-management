b93b7810464d7b7c7235580a932d192b5366f1b9
ShoroveTajmen <sorovitajmen@gmail.com>
2023-10-09 11:40:52 +0600

    Buy Ticket  page responsiveness done

--- src/Pages/BuyTicket/BuyTicket.jsx
+++ src/Pages/BuyTicket/BuyTicket.jsx
@@ -2,19 +2,17 @@ import { FaWpforms } from "react-icons/fa";
 import { RiTicket2Fill, RiTicket2Line } from "react-icons/ri";
 const BuyTicket = () => {
   return (
-    <div className="bg-blue-950">
+    <div className="bg-blue-950 h-[1700px] md:h-[1200px] mb-[1px]">
       <div className="relative w-full h-[400px]">
         {/* background image */}
         <div className="absolute top-0 right-0 bottom-0 left-0 bg-[url('https://i.ibb.co/QrTcD11/TICKET.jpg')] bg-cover bg-center bg-no-repeat h-[400px] "></div>
         {/* white overlay */}
         <div className="absolute top-0 right-0 bottom-0 left-0 bg-black opacity-60 "></div>
         <div className="relative">
-          <h1 className="absolute text-white top-[150px] left-[550px] font-bold text-5xl">
+          <h1 className="absolute text-white md:top-[150px] top-[170px] md:left-[550px] font-bold md:text-5xl text-4xl ml-2 md:ml-0">
             BUY YOUR EVENT TICKET
           </h1>
-          <h1 className="text-white absolute top-[210px] left-[690px]">
-            InnovateSpace Event Arena
-          </h1>
+          <h1 className="text-white absolute top-[230px] md:top-[210px] md:left-[690px] left-[115px]">InnovateSpace Event Arena</h1>
         </div>
       </div>
 
@@ -27,10 +25,10 @@ const BuyTicket = () => {
         </div>
       </div>
 
-      <div className="grid grid-cols-6 gap-5 text-white w-[900px] mx-auto text-center mt-10">
-        <div className="col-span-2">
+      <div className="grid md:grid-cols-6 grid-cols-1 gap-5 text-white w-[300px] md:w-[900px] mx-auto text-center mt-10">
+        <div className="col-span-2 mb-12 md:mb-0">
           <h1>
-            <FaWpforms className="w-[50px] h-[50px] text-blue-500 ml-[110px]"></FaWpforms>
+            <FaWpforms className="w-[50px] h-[50px] text-blue-500 ml-[120px] md:ml-[110px]"></FaWpforms>
           </h1>
           <h1 className="text-2xl font-bold mt-5 mb-3">Submit The Form</h1>
           <p>
@@ -39,18 +37,18 @@ const BuyTicket = () => {
             accuracy before submitting.
           </p>
         </div>
-        <div className="col-span-2">
+        <div className="col-span-2 mb-12 md:mb-0">
           <h1>
-            <RiTicket2Fill className="w-[50px] h-[50px] text-blue-500 ml-[110px]"></RiTicket2Fill>
+            <RiTicket2Fill className="w-[50px] h-[50px] text-blue-500 ml-[125px] md:ml-[110px]"></RiTicket2Fill>
           </h1>
           <h1 className="text-2xl font-bold mt-5 mb-3">Payment</h1>
           <p>
           Visit our websites payment page, choose your preferred payment method, provide payment details, including the amount, and confirm to complete the transaction.
           </p>
         </div>
-        <div className="col-span-2">
+        <div className="col-span-2 mb-12 md:mb-0">
           <h1>
-            <RiTicket2Line className="w-[50px] h-[50px] text-blue-500 ml-[110px]"></RiTicket2Line>
+            <RiTicket2Line className="w-[50px] h-[50px] text-blue-500 ml-[120px] md:ml-[110px]"></RiTicket2Line>
           </h1>
           <h1 className="text-2xl font-bold mt-5 mb-3">Get Your Ticket</h1>
           <p>
@@ -58,7 +56,7 @@ const BuyTicket = () => {
           </p>
         </div>
       </div>
-      <div className="ml-[700px] mt-5">
+      <div className="ml-[135px] md:ml-[700px] mt-5 ">
         <button className="btn btn-primary font-bold">GET TICKET NOW!</button>
       </div>
     </div>
